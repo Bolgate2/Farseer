dfa5a0b26aa04a7bc5cc317e0fa1191c48979a94
Bolgate2 <finleyjschmidt@gmail.com>
2024-01-30 02:06:29 +1100

    removed protected

--- src/rocket/components/bodyTube/bodyTube.hpp
+++ src/rocket/components/bodyTube/bodyTube.hpp
@@ -7,7 +7,6 @@ namespace Rocket{
 class BodyTube : public Component<std::tuple<>,AerodynamicCalculator<BodyTube>, KinematicCalculator<BodyTube>>{
     private:
         HollowCylinder _shape;
-    protected:
     public:
         BodyTube(HollowCylinder cyl);
         HollowCylinder shape(){ return _shape; }
