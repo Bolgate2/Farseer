8c6cd9549bcc9f682762d9ae1b17ebebc565f4fb
Bolgate2 <finleyjschmidt@gmail.com>
2024-01-29 16:21:33 +1100

    split body tube into multiple files

--- src/rocket/components/bodyTube/bodyTube.cpp
+++ src/rocket/components/bodyTube/bodyTube.cpp
@@ -4,35 +4,9 @@
 
 namespace Rocket{
 
-HollowCylinder::HollowCylinder(double radius, double height, double thickness, bool filled){
-    setRadius(radius);
-    setHeight(height);
-    setThickness(thickness);
-    setFilled(filled);
-}
-
-double HollowCylinder::volume(){
-    double vol = 0;
-    if(filled() == true){
-        vol = height() * std::numbers::pi * std::pow(radius(), 2);
-    } else {
-        vol = height() * std::numbers::pi * (2*radius()*thickness() - std::pow(thickness(),2));
-    }
-    return vol;
-}
-
 BodyTube::BodyTube(std::unique_ptr<HollowCylinder> cyl)
 {
     _shape = std::move(cyl);
-}
-
-
-BodyTubeAerodynamicCalculator::BodyTubeAerodynamicCalculator(BodyTube* comp):
-AerodynamicCalculator(comp)
-{}
-
-double BodyTubeAerodynamicCalculator::CnAlpha(double mach, double alpha){
-    return getComponent()->shape()->volume();
 }
 
 BodyTubeKinematicCalculator::BodyTubeKinematicCalculator(AbstractComponent* comp):
--- src/rocket/components/bodyTube/bodyTube.hpp
+++ src/rocket/components/bodyTube/bodyTube.hpp
@@ -1,36 +1,8 @@
 #pragma once
 #include "../component.hpp"
+#include "hollowCylinder.hpp"
 
 namespace Rocket{
-/*
-class BodyTube : public Component{
-    public:
-        BodyTube();
-        virtual void initAerodynamicCalculator() override;
-};
-*/
-
-class HollowCylinder{
-    private:
-        double _radius;
-        double _height;
-        double _thickness;
-        bool _filled;
-    public:
-        HollowCylinder(double radius, double height, double thickness, bool filled = false);
-        // getters and setters
-        double radius(){ return _radius; }
-        void setRadius(double radius){ _radius = std::max(radius, 0.0); }
-        double height(){ return _height; }
-        void setHeight(double height){ _height = std::max(height, 0.0); }
-        double thickness(){ return _thickness; }
-        void setThickness(double thickness){ _thickness = std::max(thickness, 0.0); }
-        bool filled(){ return _filled; }
-        bool setFilled(bool filled){ _filled = filled; }
-
-        double volume();
-
-};
 
 class BodyTubeAerodynamicCalculator;
 class BodyTubeKinematicCalculator;
