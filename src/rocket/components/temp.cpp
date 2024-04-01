541f7e02c55617cc034eebe05210afa31e111c8b
Bolgate2 <finleyjschmidt@gmail.com>
2024-04-01 13:58:10 +1100

    scorched earth

---
+++ src/rocket/components/temp.cpp
@@ -0,0 +1,11 @@
+#include "temp.hpp"
+
+namespace Rocket{
+
+void test(){
+    std::cout << "test success" << std::endl;
+}
+
+
+}
+
---
+++ src/rocket/components/temp.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+namespace Rocket{
+
+void test();
+
+}
